df812409d67684a04750c4a42a3eafa9e5c6f085
Elliot Dohm <elliot.dohm@shopify.com>
2017-06-23 18:29:59 -0400

    Symlink Subl 3

--- setup.sh
+++ setup.sh
@@ -4,8 +4,15 @@ install_packages() {
   bash packages/homebrew.sh
 }
 
+symlink_subl() {
+  echo "Setting up Sublime symlink..."
+  ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+  export EDITOR='subl -w'
+}
+
 begin() {
   install_packages
+  symlink_subl
 }
 
 begin "$@"
