4854f246bcf0c6824c83b379a3c15bba3269db7c
Elliot Dohm <elliot.dohm@shopify.com>
2018-03-06 21:55:39 -0500

    Add custom git publish and commit functions to .gitconfig

---
+++ common/custom_git_functions/publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+publish() {
+  # git branch-name is defined in .gitconfig
+
+  if [ $# -eq 0 ]; then
+    git push -u origin $(git branch-name);
+  elif [ "${1}" == "-f" ]; then
+    git push -f origin $(git branch-name);
+  else
+    echo "${1} is an invalid argument!";
+  fi
+}
+
+publish "$@"
