f353b9d21798bf5be36e2718b6faae0913715f78
Elliot Dohm <elliot.dohm@shopify.com>
2018-08-11 13:00:09 -0400

    Use commit.verbose instead of bash function

--- .bash/git.sh
+++
@@ -1,9 +0,0 @@
-# Bash alias don't support spaces, and default git commands cannot be realiased
-# in gitconfig. This is to set `git commit` to be verbose by default.
-function git {
-  if [[ $1 == "commit" ]]; then
-    command git commit --verbose
-  else
-    command git "$@"
-  fi
-}
