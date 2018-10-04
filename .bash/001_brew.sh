781ce6baee304b62439ff4d3a72ea20db8ba2b7f
Elliot Dohm <elliot.dohm@shopify.com>
2018-10-03 21:28:06 -0400

    Auto install brew

--- .bash/001_brew.sh
+++ .bash/001_brew.sh
@@ -1,4 +1,3 @@
-# FIXME: Expensive to run brew bundle check everytime bashrc is loaded.
-if test -f '.Brewfile' && command -v brew bundle check >/dev/null 2>&1; then
-  brew bundle
+if ! command -v brew >/dev/null 2>&1; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
