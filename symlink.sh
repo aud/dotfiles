59a34bb7f9d6795fce0433be1109186fe8d95bf6
Elliot Dohm <elliot.dohm@shopify.com>
2018-10-03 23:56:06 -0400

    Symlinker

---
+++ symlink.sh
@@ -0,0 +1,52 @@
+# If target file exists, unlink.
+function symlink {
+  ln -sfh $1 $2
+}
+
+function file_match {
+  cmp --silent $1 $2
+}
+
+function dir_match {
+  diff -r                \
+    --exclude=autoload   \
+    --exclude=.vim       \
+    --exclude=.netrwhist \
+    --exclude=plugged  $1 $2
+}
+
+function symlink_exists {
+  readlink $1 == $2 >/dev/null
+}
+
+function begin {
+  for file in .[^.]*; do
+    local path="$(pwd)/$file"
+    local base=$(basename $file)
+    local target="$HOME/$base"
+
+    # Don't symlink .git
+    if [[ $file == ".git" ]]; then
+      continue
+    fi
+
+    if test -h $target && symlink_exists $target $path; then
+      echo "$base is already symlinked."
+    elif test -f $target && file_match $path $target || dir_match $path $target; then
+      echo "$base is identical to the existing file. Overriding with symlink."
+
+      symlink $path $target
+    elif [[ -a $target ]]; then
+      read -p "$base differs from the existing file. Override? [yn] " choice
+
+      case "$choice" in
+        y|Y ) symlink $path $target;;
+      esac
+    else
+      echo "$base does not exist. Symlinking to dotfile."
+      symlink $path $target
+    fi
+  done
+}
+
+begin "$@"
