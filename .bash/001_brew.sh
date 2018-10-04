5e92dc608547f79f8bd389832eb27751cce7a7d6
Elliot Dohm <elliot.dohm@shopify.com>
2018-10-04 00:24:24 -0400

    Remove unused files

--- .bash/001_brew.sh
+++
@@ -1,3 +0,0 @@
-if ! command -v brew >/dev/null 2>&1; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
--- .bash/002_brewfile.sh
+++
@@ -1,4 +0,0 @@
-# FIXME: Expensive to run brew bundle check everytime bashrc is loaded.
-if test -f '.Brewfile' && command -v brew bundle check >/dev/null 2>&1; then
-  brew bundle
-fi
