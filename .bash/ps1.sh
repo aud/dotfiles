b78eb1635603e2f4ffa5d3773fb7bba9d0c89c0d
Elliot Dohm <elliot.dohm@shopify.com>
2020-05-21 01:39:00 -0400

    Handle repos with no commits

--- .bash/ps1.sh
+++ .bash/ps1.sh
@@ -1,6 +1,6 @@
 # Fetch current branch name and wrap in parenthesis if exists.
 parsed_git_branch() {
-  [ -d '.git' ] && echo "($(git rev-parse --abbrev-ref HEAD))"
+  [ -d '.git' ] && echo "($(git rev-parse --abbrev-ref HEAD 2>/dev/null))"
 }
 
 PS1="\W\[\e[34m\]\$(parsed_git_branch)\[\e[m\] \$ "
