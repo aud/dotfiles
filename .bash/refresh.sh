ba4446f3aee0c26966d7b8d07bb9ea48ff5b8f54
Elliot Dohm <elliot.dohm@shopify.com>
2020-10-18 21:42:07 -0400

    Refactor refresh script

--- .bash/refresh.sh
+++ .bash/refresh.sh
@@ -1,16 +1,13 @@
-printf_with_newline() {
-  printf "\n$1\n"
-}
-
 refresh() {
-  printf_with_newline "=====================starting brew runner====================="
-  printf_with_newline "running brew update..." && brew update
-  printf_with_newline "running brew upgrade..." && brew upgrade
-  printf_with_newline "running brew cleanup..." && brew cleanup
-  printf_with_newline "=====================brew runner complete====================="
-  printf_with_newline "=====================starting nvim(plug) runner====================="
-  printf_with_newline "running 'nvim --headless +PlugClean! +qall.." && nvim --headless +PlugClean! +qall
-  printf_with_newline "running 'nvim --headless +PlugUpgrade +qall.." && nvim --headless +PlugUpgrade +qall
-  printf_with_newline "running 'nvim --headless +PlugUpdate +qall.." && nvim --headless +PlugUpdate +qall
-  printf_with_newline "=====================nvim(plug) runner complete====================="
+  set -x
+
+  brew update
+  brew upgrade
+  brew cleanup
+
+  nvim --headless +PlugClean! +qall
+  nvim --headless +PlugUpgrade +qall
+  nvim --headless +PlugUpdate +qall
+
+  set +x
 }
