199d8a9a37c54bcc440155c3ea664628dad2df3c
Elliot Dohm <elliot.dohm@shopify.com>
2021-02-07 18:08:26 -0500

    Testing on vm

--- install.sh
+++ install.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
-set -euox pipefail
+set -euo pipefail
 
 echo "installing dotfiles.."
 
 if [[ $SPIN ]]; then
   echo "symlinking.."
+
+  for file in .[^.]*; do
+    from="$(pwd)/$file"
+    to="$HOME/$(basename $file)"
+
+    # Don't symlink .git
+    if [[ $file == ".git" ]]; then
+      continue
+    fi
+
+    set -x
+    ln -sf $from $to
+    set +x
+  done
+
   echo "installing deps.."
+
+  if ! command -v rg &> /dev/null; then
+    sudo apt-get install -y ripgrep
+  fi
+
+  if ! command -v htop &> /dev/null; then
+    sudo apt-get install -y htop
+  fi
+
+  if ! command -v nvim &> /dev/null; then
+    sudo apt-get install -y neovim
+  fi
 else
   if ! command -v brew >/dev/null 2>&1; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
