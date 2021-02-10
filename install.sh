f548d1f145c8e97602c17542fabe9a9ec7e53cfa
Elliot Dohm <elliot.dohm@shopify.com>
2021-02-10 17:58:03 -0500

    Remove pipefail

--- install.sh
+++ install.sh
@@ -5,56 +5,56 @@ if [[ $SPIN ]]; then
 fi
 # sudo chsh /bin/bash
 
-# # set -euo pipefail
+# set -euo pipefail
 
-# # echo "installing dotfiles.."
+# echo "installing dotfiles.."
 
-# if [ $SPIN ]; then
-#   echo "symlinking.."
+if [[ $SPIN ]]; then
+  echo "symlinking.."
 
-#   for file in .[^.]*; do
-#     from="$(pwd)/$file"
-#     to="$HOME/$(basename $file)"
+  for file in .[^.]*; do
+    from="$(pwd)/$file"
+    to="$HOME/$(basename $file)"
 
-#     # Don't symlink .git
-#     if [[ $file == ".git" ]]; then
-#       continue
-#     fi
+    # Don't symlink .git
+    if [[ $file == ".git" ]]; then
+      continue
+    fi
 
-#     set -x
-#     ln -sf $from $to
-#     set +x
-#   done
+    set -x
+    ln -sf $from $to
+    set +x
+  done
 
-#   set -x
-#   ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-#   set +x
+  set -x
+  ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+  set +x
 
-#   echo "installing deps.."
+  echo "installing deps.."
 
-#   if ! command -v rg &> /dev/null; then
-#     sudo apt-get install -y ripgrep
-#   fi
+  if ! command -v rg &> /dev/null; then
+    sudo apt-get install -y ripgrep
+  fi
 
-#   if ! command -v htop &> /dev/null; then
-#     sudo apt-get install -y htop
-#   fi
+  if ! command -v htop &> /dev/null; then
+    sudo apt-get install -y htop
+  fi
 
-#   if ! command -v nvim &> /dev/null; then
-#     sudo apt-get install -y neovim
-#   fi
-# else
-#   if ! command -v brew >/dev/null 2>&1; then
-#     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#   fi
+  if ! command -v nvim &> /dev/null; then
+    sudo apt-get install -y neovim
+  fi
+else
+  if ! command -v brew >/dev/null 2>&1; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi
 
-#   if test -f 'Brewfile' && command -v brew bundle check >/dev/null 2>&1; then
-#     brew bundle
-#   fi
+  if test -f 'Brewfile' && command -v brew bundle check >/dev/null 2>&1; then
+    brew bundle
+  fi
 
-#   source './symlink.sh'
-#   source './.bashrc'
-#   source './.system'
-# fi
+  source './symlink.sh'
+  source './.bashrc'
+  source './.system'
+fi
 
-# echo "done!"
+echo "done!"
