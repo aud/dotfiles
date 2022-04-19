cc0c26bb20476eb1a22794433f359b255f47b85a
Elliot Dohm <elliot.dohm@shopify.com>
2022-04-19 11:06:05 -0400

    comment out setup

--- install.sh
+++ install.sh
@@ -4,47 +4,23 @@
 #   echo "lol"
 # fi
 
-set -euo pipefail
+# set -euo pipefail
 
-if [ $SPIN ]; then
-  # echo "Configuring bash to default shell"
-  # sudo chsh spin -s /bin/bash
+# if [ $SPIN ]; then
+#   # echo "Configuring bash to default shell"
+#   # sudo chsh spin -s /bin/bash
 
-  if ! command -v rg &> /dev/null; then
-    sudo apt-get install -y ripgrep
-  fi
+#   if ! command -v rg &> /dev/null; then
+#     sudo apt-get install -y ripgrep
+#   fi
 
-  if ! command -v htop &> /dev/null; then
-    sudo apt-get install -y htop
-  fi
+#   if ! command -v htop &> /dev/null; then
+#     sudo apt-get install -y htop
+#   fi
 
-  if ! command -v nvim &> /dev/null; then
-    sudo apt-get install -y neovim
-  fi
-
-  for file in .[^.]*; do
-    from="$(pwd)/$file"
-    to="$HOME/$(basename $file)"
-
-    # Don't symlink .git
-    if [[ $file == ".git" ]]; then
-      continue
-    fi
-
-    set -x
-    ln -sf $from $to
-    set +x
-  done
-
-  set -x
-  ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-  set +x
-fi
-
-# # echo "installing dotfiles.."
-
-# if [[ $SPIN ]]; then
-#   echo "symlinking.."
+#   if ! command -v nvim &> /dev/null; then
+#     sudo apt-get install -y neovim
+#   fi
 
 #   for file in .[^.]*; do
 #     from="$(pwd)/$file"
@@ -63,32 +39,56 @@ fi
 #   set -x
 #   ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 #   set +x
-
-#   echo "installing deps.."
-
-#   if ! command -v rg &> /dev/null; then
-#     sudo apt-get install -y ripgrep
-#   fi
-
-#   if ! command -v htop &> /dev/null; then
-#     sudo apt-get install -y htop
-#   fi
-
-#   if ! command -v nvim &> /dev/null; then
-#     sudo apt-get install -y neovim
-#   fi
-# else
-#   if ! command -v brew >/dev/null 2>&1; then
-#     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#   fi
-
-#   if test -f 'Brewfile' && command -v brew bundle check >/dev/null 2>&1; then
-#     brew bundle
-#   fi
-
-#   source './symlink.sh'
-#   source './.bashrc'
-#   source './.system'
 # fi
 
-# echo "done!"
+# # # echo "installing dotfiles.."
+
+# # if [[ $SPIN ]]; then
+# #   echo "symlinking.."
+
+# #   for file in .[^.]*; do
+# #     from="$(pwd)/$file"
+# #     to="$HOME/$(basename $file)"
+
+# #     # Don't symlink .git
+# #     if [[ $file == ".git" ]]; then
+# #       continue
+# #     fi
+
+# #     set -x
+# #     ln -sf $from $to
+# #     set +x
+# #   done
+
+# #   set -x
+# #   ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+# #   set +x
+
+# #   echo "installing deps.."
+
+# #   if ! command -v rg &> /dev/null; then
+# #     sudo apt-get install -y ripgrep
+# #   fi
+
+# #   if ! command -v htop &> /dev/null; then
+# #     sudo apt-get install -y htop
+# #   fi
+
+# #   if ! command -v nvim &> /dev/null; then
+# #     sudo apt-get install -y neovim
+# #   fi
+# # else
+# #   if ! command -v brew >/dev/null 2>&1; then
+# #     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# #   fi
+
+# #   if test -f 'Brewfile' && command -v brew bundle check >/dev/null 2>&1; then
+# #     brew bundle
+# #   fi
+
+# #   source './symlink.sh'
+# #   source './.bashrc'
+# #   source './.system'
+# # fi
+
+# # echo "done!"
