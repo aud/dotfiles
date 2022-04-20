bbec65f00bae9612a591dd8f6e9408d842e58260
Elliot Dohm <elliot.dohm@shopify.com>
2022-04-19 22:44:38 -0400

    link nvim

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euox pipefail
 
 if [ $SPIN ]; then
   sudo add-apt-repository -y ppa:neovim-ppa/stable
@@ -26,61 +26,7 @@ if [ $SPIN ]; then
   alias vim="nvim"
   alias c="clear"
   alias ls="ls -Ga"
+
+  mkdir -p $HOME/.config/nvim
   ln -sf $HOME/dotfiles/.config/nvim/init.vim $HOME/.config/nvim/init.vim
 fi
-
-
-  # set -x
-  # set +x
-
-# # # echo "installing dotfiles.."
-
-# # if [[ $SPIN ]]; then
-# #   echo "symlinking.."
-
-# #   for file in .[^.]*; do
-# #     from="$(pwd)/$file"
-# #     to="$HOME/$(basename $file)"
-
-# #     # Don't symlink .git
-# #     if [[ $file == ".git" ]]; then
-# #       continue
-# #     fi
-
-# #     set -x
-# #     ln -sf $from $to
-# #     set +x
-# #   done
-
-# #   set -x
-# #   ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-# #   set +x
-
-# #   echo "installing deps.."
-
-# #   if ! command -v rg &> /dev/null; then
-# #     sudo apt-get install -y ripgrep
-# #   fi
-
-# #   if ! command -v htop &> /dev/null; then
-# #     sudo apt-get install -y htop
-# #   fi
-
-# #   if ! command -v nvim &> /dev/null; then
-# #     sudo apt-get install -y neovim
-# #   fi
-# # else
-# #   if ! command -v brew >/dev/null 2>&1; then
-# #     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# #   fi
-
-# #   if test -f 'Brewfile' && command -v brew bundle check >/dev/null 2>&1; then
-# #     brew bundle
-# #   fi
-
-# #   source './symlink.sh'
-# #   source './.bashrc'
-# #   source './.system'
-# # fi
-
-# # echo "done!"
