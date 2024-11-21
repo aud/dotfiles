48bd62888050b4f82a3503b1a72e83146a38d525
Elliot Dohm <elliotdohm@gmail.com>
2024-11-21 02:19:58 -0500

    tweaks

--- scripts/kill.rb
+++ scripts/kill.rb
@@ -1,47 +1,38 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby --disable-gems
 # frozen_string_literal: true
 
-require "open3"
-require "json"
-
-
-activity = false
 threads = []
 
 # Kill any matching processes
-%w(
-  ruby
-  python
-  node
-  puma
-  sidekiq
-).each do |process_name|
-  stdout, _stderr, _status = Open3.capture3("pgrep -if #{process_name} | xargs ps")
+[
+  "ruby",
+  "python",
+  "node",
+  "puma",
+  "sidekiq",
+].each do |process_name|
+  stdout = `pgrep -if #{process_name} | xargs ps`
 
   # Remove column headers
   processes = stdout.split("\n")[1..]
   next if processes.nil?
 
   processes.each do |process|
-    activity = true
+    p = process.split(" ")
+    pid = p[0]
+    initiating_command = p[4..].join(' ')
 
-    process.split(" ").tap do |p|
-      pid = p[0]
-      initiating_command = p[4..].join(' ')
-
-      threads << Thread.new do
-        puts "sigkill process: #{initiating_command}"
-        _stdout, stderr, status = Open3.capture3("kill -9 #{pid}")
-
-        puts stderr unless status.success?
-      end
+    threads << Thread.new do
+      puts "sigkill process: #{initiating_command}"
+      result = `kill -9 #{pid} 2>&1`
+      puts result unless $?.success?
     end
   end
 end
 
-if activity
+if threads.empty?
+  puts "nothing todo"
+else
   threads.each(&:join)
   puts "done"
-else
-  puts "nothing todo"
 end
